e94cc1b72701b663231b1f3458a740498bbf15df
maheshhp <hpmahesh73@gmail.com>
2018-02-03 20:13:41 +0530

    Added Hapi server

---
+++ server.js
@@ -0,0 +1,21 @@
+const Hapi = require('hapi');
+const Routes = require('./routes');
+
+const server = new Hapi.Server();
+server.connection({
+  port: 8080,
+  host: 'localhost',
+});
+
+server.route(Routes);
+
+if (!module.parent) {
+  server.start((error) => {
+    if (error) {
+      throw (error);
+    }
+    console.log('Server started at port 8080');
+  });
+}
+
+module.exports = server;
