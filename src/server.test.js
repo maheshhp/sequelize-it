1a5857ef96437d625f6b3a4ef05d9486ddf8ae6b
maheshhp <hpmahesh73@gmail.com>
2018-02-04 15:01:57 +0530

    server - Added test for response code on GET request for successful request

--- src/server.test.js
+++ src/server.test.js
@@ -5,4 +5,15 @@ describe('Testing the Hapi server that processes the requests', () => {
   test('Should contain correct number of routes', () => {
     expect(Routes.length).toBe(Server.table('localhost')[0].table.length);
   });
+  test('Should return 200 status code for sucessful request', (done) => {
+    const request = {
+      method: 'GET',
+      url: '/users',
+    };
+    Server.inject(request, (response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+    expect(Routes.length).toBe(Server.table('localhost')[0].table.length);
+  });
 });
